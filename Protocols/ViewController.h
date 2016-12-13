f6fca57dcd8219e5ac0c416d5f35138e6e57fcad
Denis Mordvinov <denis.mordvinov@rosberry.com>
2016-12-13 18:50:15 +0600

    Initial Commit

---
+++ Protocols/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Protocols
+//
+//  Created by Denis Mordvinov on 13.12.16.
+//  Copyright © 2016 Rosberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
